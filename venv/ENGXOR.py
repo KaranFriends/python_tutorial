23d8b9aac398747b248f3c683a3eac5c989d7b31
KaranFriends <karanrajsinghranawat@gmail.com>
2020-03-10 00:25:05 +0530

    engxor:  time complexity problem again: new io but still no progress, may be there is some error in logic

--- venv/ENGXOR.py
+++ venv/ENGXOR.py
@@ -1,53 +1,49 @@
-import atexit, io, sys
-
-# A stream implementation using an in-memory bytes
-# buffer. It inherits BufferedIOBase.
-buffer = io.BytesIO()
-sys.stdout = buffer
-
-
-# print via here
-@atexit.register
-def write():
-    sys.__stdout__.write(buffer.getvalue())
-
-
-#####################################
-# template ends
-
-# normal method followed
-# input N
-# n = int(raw_input())
-
-# input the array
-
-def countOne(n):
-    ones = 0
-    # print(n)
-    while n > 0:
-        ones = ones + int(n%2)
-        n = n/2
-    # print(ones)
-    return ones
+def main():
+    from sys import stdin, stdout
+    # n, k = stdin.readline().split()
+    # n = int(n)
+    # k = int(k)
+    #
+    # cnt = 0
+    # lines = stdin.readlines()
+    # for line in lines:
+    #     if int(line) % k == 0:
+    #         cnt += 1
+    #
+    # stdout.write(str(cnt))
 
 
-t = int(input())
+    def countOne(n):
+        ones = 0
+        # print(n)
+        while n > 0:
+            ones = ones + int(n%2)
+            n = n/2
+        # print(ones)
+        return ones
 
-while t > 0:
-    m, n=input().split()
-    m=int(m)
-    n=int(n)
-    f = [int(x) for x in input().split()]
-    # f = list(map(int, input().strip().split()))[:m]
-    while n > 0:
-        odd = 0
-        even = 0
-        p=int(input())
-        for i in f:
-            if countOne(i+(p*(i+p))) % 2 == 0 :
-                even+=1
-            else:
-                odd+=1
-        print(even.__str__()+" "+odd.__str__())
-        n-=1
-    t-=1
+
+    t = int(stdin.readline())
+
+    while t > 0:
+        m, n = stdin.readline().split()
+        m=int(m)
+        n=int(n)
+        f = [int(x) for x in stdin.readline().split()]
+        # f = list(map(int, input().strip().split()))[:m]
+        while n > 0:
+            odd = 0
+            even = 0
+            p=int(input())
+            for i in f:
+                if countOne(i+(p*(i+p))) % 2 == 0 :
+                    even+=1
+                else:
+                    odd+=1
+            stdout.write(even.__str__()+" "+odd.__str__())
+            n-=1
+        t-=1
+
+if __name__ == "__main__":
+    main()
+
