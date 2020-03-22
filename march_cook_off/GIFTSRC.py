38225e4296d58ad7f48b9d6ce48c7d58ad56b1fc
KaranFriends <karanrajsinghranawat@gmail.com>
2020-03-23 00:21:24 +0530

    done

--- march_cook_off/GIFTSRC.py
+++ march_cook_off/GIFTSRC.py
@@ -6,31 +6,54 @@ while t > 0:
     n=int(stdin.readline())
     m = stdin.readline()
     m=list(m.rstrip())
-    # ans=[]
-    print(m)
-    for i in m:
-        if m.index(i)==(m.__len__()-1):
-            break
-        print(m[m.index(i)],  m[m.index(i)+1])
-        if m[m.index(i)] == m[int(m.index(i))+1]:
-            m=m[:int(m.index(i))+1]+m[int(m.index(i))+2:]
-            #m.remove(i)
-            print("duplicate removed")
+    ans=[]
+    # print(m)
 
-        if i=="L" and m[m.index(i)+1] =="R":
-            m.remove(m[m.index(i)+1])
-            print("R removed")
-        elif i=="R" and m[m.index(i)+1] =="L":
-            m.remove(m[m.index(i)+1])
-            print("L removed")
-        elif i=="U" and m[m.index(i)+1] =="D":
-            m.remove(m[m.index(i)+1])
-            print("D removed")
-        elif i == "D" and m[m.index(i) + 1] == "U":
-            m.remove(m[m.index(i) + 1])
-            print("U removed")
+    for i in range(n):
+        if i == 0:
+            ans.append(m[i])
+            continue
+
+        if m[i] == m[i - 1]:
+            continue
+
+        if m[i]=="L" and m[i-1] =="R":
+            continue
+        elif m[i]=="R" and m[i-1] =="L":
+            continue
+        elif m[i]=="U" and m[i-1] =="D":
+            continue
+        elif m[i]=="D" and m[i-1] =="U":
+            continue
+
+        ans.append(m[i])
+
+    # print(ans)
+
+    # for i in m:
+    #     if m.index(i) == 0:
+    #         continue
+    #     print(m[m.index(i)-1],i)
+    #
+    #     if i == m[m.index(i)-1]:
+    #         # m=m[:int(m.index(i))] + m[int(m.index(i))+1:]
+    #         # m.remove(i)
+    #         print("duplicate removed")
+    #
+    #     # if i=="L" and m[m.index(i)+1] =="R":
+    #     #     m.remove(m[m.index(i)+1])
+    #     #     print("R removed")
+    #     # elif i=="R" and m[m.index(i)+1] =="L":
+    #     #     m.remove(m[m.index(i)+1])
+    #     #     print("L removed")
+    #     # elif i=="U" and m[m.index(i)+1] =="D":
+    #     #     m.remove(m[m.index(i)+1])
+    #     #     print("D removed")
+    #     # elif i == "D" and m[m.index(i) + 1] == "U":
+    #     #     m.remove(m[m.index(i) + 1])
+    #     #     print("U removed")
     x,y=0,0
-    for i in m:
+    for i in ans:
         if i=="L":
            x-=1
         elif i=="R":
