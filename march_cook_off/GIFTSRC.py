0acaed94c14bdee8ec301892630640da6ba33b39
KaranFriends <karanrajsinghranawat@gmail.com>
2020-03-23 00:04:00 +0530

    rubbish

--- march_cook_off/GIFTSRC.py
+++ march_cook_off/GIFTSRC.py
@@ -4,23 +4,31 @@ t = int(input())
 # type(t)
 while t > 0:
     n=int(stdin.readline())
-    m = stdin.readline().split("")
-    ans=[]
-
+    m = stdin.readline()
+    m=list(m.rstrip())
+    # ans=[]
+    print(m)
     for i in m:
         if m.index(i)==(m.__len__()-1):
             break
-        if m[m.index(i)] == m[m.index(i)+1]:
-            m.remove(i)
+        print(m[m.index(i)],  m[m.index(i)+1])
+        if m[m.index(i)] == m[int(m.index(i))+1]:
+            m=m[:int(m.index(i))+1]+m[int(m.index(i))+2:]
+            #m.remove(i)
+            print("duplicate removed")
 
         if i=="L" and m[m.index(i)+1] =="R":
             m.remove(m[m.index(i)+1])
+            print("R removed")
         elif i=="R" and m[m.index(i)+1] =="L":
             m.remove(m[m.index(i)+1])
+            print("L removed")
         elif i=="U" and m[m.index(i)+1] =="D":
             m.remove(m[m.index(i)+1])
+            print("D removed")
         elif i == "D" and m[m.index(i) + 1] == "U":
             m.remove(m[m.index(i) + 1])
+            print("U removed")
     x,y=0,0
     for i in m:
         if i=="L":
