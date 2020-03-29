b303e690e2b6d633902bbadb44f53309f50901ad
KaranFriends <karanrajsinghranawat@gmail.com>
2020-03-30 03:32:15 +0530

    SWPDGT.py : Done

--- march_cook_off/march_lunchtime/SWPDGT.py
+++ march_cook_off/march_lunchtime/SWPDGT.py
@@ -12,12 +12,19 @@ while t>0:
     b2=b%10
     b1=int(b/10)
 
-    print(a1, a2, b1, b2)
-
-    A=b1*10 + a1
-    B=b2*10 + a2
-
-    AA=a1*10 + b1
-    BB=a2*10 + b2
-    print(A+B, AA+BB, a+b)
-    print(max(A+B,AA+BB,a+b))
+    if b1==0 or a1==0:
+        if b1==0 and a1==0:
+            print(a+b)
+            continue
+        elif a1==0 and b1!=0:
+            print(max((a2*10 + b2)+b1,a+b))
+            continue
+        elif a1!=0 and b1==0:
+            print(max((b2 * 10 + a2) + a1, a + b))
+            continue
+    else:
+        A=b1*10 + a1
+        B=b2*10 + a2
+        AA=a1*10 + b1
+        BB=a2*10 + b2
+        print(max(A+B,AA+BB,a+b))
