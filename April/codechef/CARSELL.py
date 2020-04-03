a767412dd6ab40ea35deb5ece2538202fb02b8c3
KaranFriends <karanrajsinghranawat@gmail.com>
2020-04-03 22:07:58 +0530

    compilation done, successfully submitted

--- April/codechef/CARSELL.py
+++ April/codechef/CARSELL.py
@@ -1,11 +1,22 @@
 from queue import PriorityQueue
 from sys import stdin, stdout
 
-t=int(stdin.readline())
-while t>0:
-    t-=1
+tt=int(stdin.readline())
+while tt>0:
+    # print(t)
+    tt-=1
     q=PriorityQueue()
     t = int(stdin.readline())
-    map(lambda x: q.put(-int(x)),stdin.readline().rsplit())
-    # heapq.heapify(p)
-    print(q.get())
+    li = stdin.readline().rsplit()
+    for i in range(t):
+        q.put(-int(li[i]))
+    temp=0
+    ans=0
+    for i in range(t):
+        e=-q.get()-temp
+        if e<=0:
+            continue
+        else:
+            ans=ans+e
+        temp+=1
+    print(ans%(10**9+7))
