1d5c911cd529745cc567603fe06907ba64338f96
KaranFriends <karanrajsinghranawat@gmail.com>
2020-04-03 23:42:20 +0530

    problem solved

--- April/codechef/COVIDLQ.py
+++ April/codechef/COVIDLQ.py
@@ -1,20 +1,21 @@
 from sys import stdin, stdout
 
 tt=int(stdin.readline())
-while True:
+while tt>0:
     tt -= 1
-    # u = stdin.readline()
-    li = stdin.readline()
-        # .replace(" ","")
+    u = stdin.readline()
+    li = stdin.readline().replace(" ","")
     # print(li)
-    li=li.strip("0")
-    # print(li)
+    li=li[:-1]
+    li = li.lstrip('0')
+    li = li.rstrip('0')
+    # print(li,"len", len(li))
     l,r=0,0
     flag=0
     if li.count("1") == 1:
         print("YES")
         continue
-    while len(li)!=2:
+    while len(li)!=1:
         # print("length   ",len(li))
         # print("asdasdasd", li)
         li=li[1:]
