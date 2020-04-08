9278e084c8e241f0ff9b21cc797bef03b6095a4a
KaranFriends <karanrajsinghranawat@gmail.com>
2020-04-09 02:30:39 +0530

    TLE again, previous should have given wrong answer as .index works in case of string

--- Leetcode_April/happyNumber.py
+++ Leetcode_April/happyNumber.py
@@ -1,17 +1,8 @@
-
-def some_funcction(track, e):
-    try:
-        track.index(e)
-        return True
-    except:
-        return False
-
-
 class Solution:
     def isHappy(self, n: int) -> bool:
         track =[]
         while True:
-            if some_funcction(track, n):
+            if track.count(n) > 1:
                 return False
             else :
                 if n==1:
